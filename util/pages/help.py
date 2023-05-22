2fd6d9fa68ebe4af46794fba2c5d1c7fc75d6f25
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-22 20:20:28 +0530

    Update help.py

--- util/pages/help.py
+++ util/pages/help.py
@@ -1,9 +1,43 @@
 import streamlit as st
 
 def help():
-  st.write("# Something You Should See")
+  st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"][aria-expanded="true"]{
+            background-color: #b388eb;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
   
-  st.write("## How to obtain your OpenAI API Key!!!")
+  st.write("# How to obtain your OpenAI API Key!!!")
+  st.write("""***""")
   
-  st.write("Step 1: Creating your OpenAI Account")
+  st.write("## (I) Creating your OpenAI Account")
+  
+  st.write(" 1) Visit the [OpenAI website](https://openai.com/)")
+  st.write(" 2) Click on the "Sign Up" or "Get Started" button to create an account.")
+  st.write(" 3) Follow the instructions to create your account by providing the required information.")
+  
+  st.write("""***""")
+  
+  st.write("## (II) Navigating to your OpenAI API")
+  
+  st.write(" 1) Once you have successfully created your account, search for "OpenAI API Key" on google and click on first link or click [here](https://platform.openai.com/account/api-keys).")
+  st.write(" 2) Login with your details.")
+  st.write(" 3) On the main landing page under the heading "API keys" you will find a button saying "Create new secret key". Click on it")
+  st.write(" 4) You will be asked to enter a name. Give any name according to your own interest.")
+  st.write(" 5) Once you have given the name then click on "Create secret key".")
+  st.write(" 6) Once you click on "Create secret key" you will get a new pop-up with your secret key. Your secret key will start with something like - "sk-<alpha_numeric characters>".")
+  st.write(" 7) Congratulations!!! You have successfully created your OpenAI API Key. Copy this key and paste it when "ARIAS" ask you to enter your API".)
+  
+  st.info("Review the pricing and usage details to understand the costs associated with using the API. Make sure to securely store your API key." 
+          "Treat it like a password and avoid sharing it with unauthorized individuals.")
+
+  
+           
+  
+  
   

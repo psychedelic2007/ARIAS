48caa6ea47542843adf5f0fdd769a33cc0e89ad5
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-22 19:53:11 +0530

    Update arias.py

--- arias.py
+++ arias.py
@@ -6,6 +6,7 @@ from multiapp import MultiApp
 from util.pages.home_page import home_page
 from util.pages.arias_streamlit import arias_sum
 from util.pages.ask_pdf import arias_ask
+from util.pages.help import help
 
 
 app = MultiApp()
@@ -13,5 +14,6 @@ app = MultiApp()
 app.add_app("Home Page", home_page)
 app.add_app("ARIAS Summarizer", arias_sum)
 app.add_app("ARIAS Ask", arias_ask)
+add.add_app("Help", help)
 
 app.run()

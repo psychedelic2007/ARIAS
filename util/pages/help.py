f3e649df1e1652668b6d8538c7ef9d6d86e00ccf
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-22 19:45:33 +0530

    Create help.py

---
+++ util/pages/help.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def help():
+  st.write("# Something You Should See")
+  
+  st.write("## How to obtain your OpenAI API Key!!!")
+  
+  st.write("Step 1: Creating your OpenAI Account")
+  
